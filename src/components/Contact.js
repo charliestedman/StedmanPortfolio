8c4050e4939146980f9c3d53108c3177e19bf463
Charlie Stedman <9369467+cbs9646@users.noreply.github.com>
2022-01-24 19:26:04 -0600

    added contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,28 @@
-export default function Contact() {}
+import React from "react";
+
+
+export default function Contact() {
+    return(
+        <section id="contact" className="relative">
+        <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                    Github: 
+                </h2>
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                    Email: charliestedman@yahoo.com
+                </h2>
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                    Resume: 
+                </h2>
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                    Phone: 817.253.7561
+                </h2>
+            </div>
+        </div>
+        </section>
+    )
+
+
+
+}
