2b65914d5ab5b50425503ac7e26bbb486d03d55c
Charlie Stedman <cbs9646@gmail.com>
2022-04-01 13:50:38 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-import Skills from "./components/Skills";
+import Skills from "./src/components/Skills";
 import Projects from "./components/Projects";
 
 export default function App() {
