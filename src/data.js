c4b5f9857985101196e5ccdbfa82c8b2c407e096
Charlie Stedman <cbs9646@gmail.com>
2022-04-12 11:52:12 -0500

    updated projects

--- src/data.js
+++ src/data.js
@@ -1,46 +1,34 @@
 export const projects = [
   {
     title: "Climb Base!",
-    subtitle: "App that was created to bring climbers together!",
-    description: "See above",
+    subtitle: "",
+    description: "App that was created to bring climbers together!",
     image:
       "./screenshot-hidden-wave-22130.herokuapp.com-2022.03.22-12_55_07.png",
     link: "https://hidden-wave-22130.herokuapp.com/",
   },
   {
-    title: "Note Taker",
-    subtitle: "App that was created to take notes",
-    description: "See above",
+    title: "Handy Notes",
+    subtitle: "",
+    description:
+      "Handy Notes was created to help save your important notes! You can add new notes, save locally and then return to the website later to read them. Once you are done with a note, just hit the delete button. Handy Notes was created using Heroku, JavaScript, Node.js and Express.js. Check it out on Heroku by clicking here! ",
     image: "./notes.png",
     link: "https://pacific-reaches-24360.herokuapp.com/",
   },
   {
-    title: "Team Profile Generator",
-    subtitle: "App that was created to Generate profiles",
-    description: "See above",
-    image: "./team profile generator.png",
-    link: "https://github.com/charliestedman/TeamProfileGenerator",
-  },
-  {
-    title: "Climb Base!",
-    subtitle: "App that was created to bring climbers together!",
-    description: "See above",
+    title: "WorkDay Scheduler",
+    subtitle: "",
+    description: "Helps keep your schedule",
     image:
       "./screenshot-hidden-wave-22130.herokuapp.com-2022.03.22-12_55_07.png",
     link: "https://hidden-wave-22130.herokuapp.com/",
   },
   {
-    title: "Note Taker",
-    subtitle: "App that was created to take notes",
-    description: "See above",
-    image: "./Note Taker.gif",
-    link: "https://pacific-reaches-24360.herokuapp.com/",
-  },
-  {
-    title: "Team Profile Generator",
-    subtitle: "App that was created to Generate profiles",
-    description: "See above",
-    image: "./team profile generator.png",
+    title: "Lineup ",
+    subtitle: "",
+    description:
+      "Lineup is useful for creating a managment system for employees. You can add new employees by position and update their email, github name, phone number and more! This app was created using Node.js, JavaScript and materialize.ui. Check it out on Github by clicking here!",
+    image: "./TextEditorGIF.gif",
     link: "https://github.com/charliestedman/TeamProfileGenerator",
   },
 ];
