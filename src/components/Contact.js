92d8c125763aa6cb3d05413d942ced48b58da3a4
Charlie Stedman <cbs9646@gmail.com>
2022-04-19 12:41:32 -0500

    updated contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,17 +6,19 @@ export default function Contact() {
       <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
         <div className="lg:w-1/2 px-6">
           <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-            Github:
-            <a href="www.github.com/charliestedman">
-              github.com/charliestedman
+            <div>
+              <a href="https://www.github.com/charliestedman">
+                Github: github.com/charliestedman
+              </a>
+            </div>
+          </h2>
+          <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+            Email: charliestedman@yahoo.com
+          </h2>
+          <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+            <a href="https://docs.google.com/document/d/1TNTqBjIW5K7HAxhq8ZmitiyoOfjiKqc55lrUHJ4oEUI/edit?usp=sharing">
+              Resume
             </a>
-          </h2>
-          <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-            Email:
-            <a href="charliestedman@yahoo.com">charliestedman@yahoo.com</a>
-          </h2>
-          <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-            Resume:
           </h2>
           <h2 className="title-font font-semibold text-white tracking-widest text-xs">
             Phone: 817.253.7561
