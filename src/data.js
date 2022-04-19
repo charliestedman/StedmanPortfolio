5f594952cf35cf60b3ce58adab36a9b91c7fb5d2
Charlie Stedman <cbs9646@gmail.com>
2022-04-19 12:05:14 -0500

    updated projects

--- src/data.js
+++ src/data.js
@@ -4,8 +4,7 @@ export const projects = [
     subtitle: "",
     description:
       "Climb Base was created to bring rock climbers together. Select a state, area and wall to add comments and rate rock climbing routes! Site features a user creation section, login and profile pages showing your routes that have been rated. Climb Base was created using JavaScript, MongoDB, Heroku, Express.js, Node.js and Handlebars. Click here to check it out on Heroku!",
-    image:
-      "./screenshot-hidden-wave-22130.herokuapp.com-2022.03.22-12_55_07.png",
+    image: "./Climb Base.gif",
     link: "https://hidden-wave-22130.herokuapp.com/",
   },
   {
@@ -20,17 +19,16 @@ export const projects = [
     title: "Schedule IT",
     subtitle: "",
     description:
-      "Schedule IT helps keep you on track for your workday. Select a time, enter a note about your meeting and then hit save. Current/Future time is tracked throughout the day by red, grey or green bars and will show you what times you have open! ScheduleIT was created using JavaScript, JQUERY, Bootstrap and MOMENT.js. Please click on this picture to check it out on Heroku! ",
-    image:
-      "./screenshot-hidden-wave-22130.herokuapp.com-2022.03.22-12_55_07.png",
-    link: "https://hidden-wave-22130.herokuapp.com/",
+      "Schedule IT helps keep you on track for your workday. Select a time, enter a note about your meeting and then hit save. Current/Future time is tracked throughout the day by red, grey or green bars and will show you what times you have open! ScheduleIT was created using JavaScript, JQUERY, Bootstrap and MOMENT.js. Please click on this picture to check out the repo on GITHUB! ",
+    image: "./schedule IT.gif",
+    link: "https://github.com/charliestedman/ScheduleKeeper",
   },
   {
     title: "Lineup ",
     subtitle: "",
     description:
       "Lineup is useful for creating a managment system for employees. You can add new employees by position and update their email, github name, phone number and more! This app was created using Node.js, JavaScript and materialize.ui. Check it out on Github by clicking here!",
-    image: "./TextEditorGIF.gif",
+    image: "./team profile generator.gif",
     link: "https://github.com/charliestedman/TeamProfileGenerator",
   },
 ];
